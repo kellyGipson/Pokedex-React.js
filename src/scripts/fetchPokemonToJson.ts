3d572e72b85a8726bf39e07b0126e1e82450b8af
Kelly Gipson <thomaskellygipson@gmail.com>
2022-02-26 08:43:14 -0500

    Completed fetching the entirety of the pokemon and exported it to a JSON file.

--- src/scripts/fetchPokemonToJson.ts
+++ src/scripts/fetchPokemonToJson.ts
@@ -1,40 +1,9 @@
 const axios = require('axios')
 const fs = require('fs')
 
-// function sleep(milliseconds: number) {
-//   const date = Date.now()
-//   let currentDate = null;
-//   do {
-//     currentDate = Date.now()
-//   } while (currentDate - date < milliseconds)
-// }
-
-// type pokemonInfo = {
-//   pokemonSpriteURL: string,
-//   pokemonShinySpriteURL: string,
-//   pokemonName: string,
-//   pokemonTypeOne: string,
-//   pokemonTypeTwo: string | null,
-//   pokemonAbilities: {
-//     pokemonAbilityOne: string,
-//     pokemonAbilityOneIsHidden: boolean,
-//     pokemonAbilityTwo: string | null,
-//     pokemonAbilityTwoIsHidden: boolean,
-//     pokemonAbilityThree: string | null,
-//     pokemonAbilityThreeIsHidden: boolean,
-//   },
-//   pokemonHeight: number,
-//   pokemonWeight: number,
-//   pokemonStats: {
-//       name: string,
-//   }[],
-// }[]
-
-// let pokemonList:pokemonInfo = []
-
 function fetchPokemonList() {
   let responses = []
-  for (let i = 1; i <= 1; i++) {
+  for (let i = 1; i <= 898; i++) {
     let response = axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
     responses.push(response)
   }
@@ -46,48 +15,72 @@ const awaitJson = (responses) => Promise.all(responses.map(response => {
   throw new Error(response.status)
 }))
 
+let pokemonList = []
+
 fetchPokemonList()
   .then(awaitJson)
   .then(data => {
     axios.get('https://pokeapi.co/')
       .then(response => {
-        console.log(data)
         if(response.status === 200) return data
         throw new Error(response.status)
+      })
+      .then(data => {
+        const pokemonData: Array<any> = data
+        pokemonData.forEach((pokemon, idx) => {
+          const key = pokemon.id
+          const pokemonName = pokemon.name
+          const pokemonSpriteURL = pokemon.sprites.front_default
+          const pokemonShinySpriteURL = pokemon.sprites.front_shiny
+          const pokemonTypeOne = pokemon.types[0].type.name
+          const pokemonTypeTwo = (pokemon.types[1]) ? pokemon.types[1].type.name : null
+          const pokemonAbilityOne = pokemon.abilities[0].ability.name
+          const pokemonAbilityOneIsHidden = pokemon.abilities[0].is_hidden
+          const pokemonAbilityTwo = (pokemon.abilities[1]) ? pokemon.abilities[1].ability.name : null
+          const pokemonAbilityTwoIsHidden = (pokemon.abilities[1]) ? pokemon.abilities[1].is_hidden : null
+          const pokemonAbilityThree = (pokemon.abilities[2]) ? pokemon.abilities[2].ability.name : null
+          const pokemonAbilityThreeIsHidden = (pokemon.abilities[2])? pokemon.abilities[2].is_hidden : null
+          const pokemonHeight = pokemon.height
+          const pokemonWeight = pokemon.weight
+          const pokemonStatHp  = pokemon.stats[0].base_stat
+          const pokemonStatAtt = pokemon.stats[1].base_stat
+          const pokemonStatDef = pokemon.stats[2].base_stat
+          const pokemonStatSpA = pokemon.stats[3].base_stat
+          const pokemonStatSpD = pokemon.stats[4].base_stat
+          const pokemonStatSpd = pokemon.stats[5].base_stat
+          const fetchedPokemonInfo = {
+            key,
+            pokemonName, 
+            pokemonSpriteURL, 
+            pokemonShinySpriteURL, 
+            pokemonTypeOne,
+            pokemonTypeTwo,
+            pokemonAbilityOne, 
+            pokemonAbilityOneIsHidden,
+            pokemonAbilityTwo,
+            pokemonAbilityTwoIsHidden,
+            pokemonAbilityThree,
+            pokemonAbilityThreeIsHidden,
+            pokemonHeight, 
+            pokemonWeight, 
+            pokemonStatHp,
+            pokemonStatAtt,
+            pokemonStatDef,
+            pokemonStatSpA,
+            pokemonStatSpD,
+            pokemonStatSpd,
+          }
+          pokemonList.push(fetchedPokemonInfo)
+        })
+        return pokemonList
+      })
+      .then(pokemonList => {
+        fs.writeFile('./src/assets/test.json', JSON.stringify(pokemonList), err => {
+          if (err) {
+            console.error(err)
+          }
+        })
       })
   }).catch(function handleError(error) {
     console.log(error)
   })
-
-// for (let i = 1; i <= 898; i++) {
-//   sleep(20)
-//   axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-//   .then((res) => {
-//   sleep(20)
-//   // assigning json values to local variables
-//     let key = i
-//     let pokemonName = res.data.name
-//     let pokemonSpriteURL = res.data.sprites.front_default
-//     let pokemonShinySpriteURL = res.data.sprites.front_shiny
-//     let pokemonTypes = res.data.types
-//     let pokemonAbilities = res.data.abilities
-//     let pokemonHeight = res.data.height
-//     let pokemonWeight = res.data.weight
-//     let pokemonStats = res.data.stats
-//     const fetchedPokemonInfo = {
-//       key,
-//       pokemonName, 
-//       pokemonSpriteURL, 
-//       pokemonShinySpriteURL, 
-//       pokemonTypes, 
-//       pokemonAbilities, 
-//       pokemonHeight, 
-//       pokemonWeight, 
-//       pokemonStats
-//     }
-//     pokemonList.push(fetchedPokemonInfo)
-//     fs.writeFile('./src/assets/test.json', JSON.stringify(pokemonList), err => {
-//       if (err) {
-//         console.error(err)
-//       }
-//     })
